7f938c1c5bcf146702743aaea4cbba9b0e87f290
vovarudeychuk <vovarudeychuk92@gmail.com>
2023-05-17 16:31:18 -0400

    auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,28 +4,19 @@ import User from '../models/User.js'
 
 /* REGISTER USER */
 export const register = async (req, res) => {
-  const {
-    firstName,
-    lastName,
-    email,
-    password,
-    picturePath,
-    friends,
-    location,
-    occupation,
-  } = req.body
-
-  if(!email || !password) {
-    return res.status(400).json({ msg: 'Please enter all required fields.' });
-  }
-
   try {
-    const existingUser = await User.findOne({ email })
-    if(existingUser) {
-      return res.status(400).json({ msg: 'An account with this email already exists.' });
-    }
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body
 
-    const salt = await bcrypt.genSalt(Number(process.env.SALT_ROUNDS || 10))
+    const salt = await bcrypt.genSalt()
     const passwordHash = await bcrypt.hash(password, salt)
 
     const newUser = new User({
@@ -40,17 +31,8 @@ export const register = async (req, res) => {
       viewedProfile: Math.floor(Math.random() * 10000),
       impressions: Math.floor(Math.random() * 10000),
     })
-
     const savedUser = await newUser.save()
-    res.status(201).json({
-      user: {
-        id: savedUser._id,
-        firstName: savedUser.firstName,
-        lastName: savedUser.lastName,
-        email: savedUser.email,
-        // send any more user fields you want to share
-      }
-    })
+    res.status(201).json(savedUser)
   } catch (err) {
     res.status(500).json({ error: err.message })
   }
@@ -58,30 +40,17 @@ export const register = async (req, res) => {
 
 /* LOGGING IN */
 export const login = async (req, res) => {
-  const { email, password } = req.body
-  if(!email || !password) {
-    return res.status(400).json({ msg: 'Please enter all required fields.' });
-  }
-
   try {
-    const user = await User.findOne({ email })
-    if (!user) return res.status(400).json({ msg: 'No account with this email exists.' })
+    const { email, password } = req.body
+    const user = await User.findOne({ email: email })
+    if (!user) return res.status(400).json({ msg: 'User does not exist. ' })
 
     const isMatch = await bcrypt.compare(password, user.password)
-    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials.' })
+    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials. ' })
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
-    
-    res.status(200).json({
-      token,
-      user: {
-        id: user._id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        // send any more user fields you want to share
-      }
-    })
+    delete user.password
+    res.status(200).json({ token, user })
   } catch (err) {
     res.status(500).json({ error: err.message })
   }
